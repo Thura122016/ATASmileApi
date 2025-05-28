3ccb45ff9d04ef06c755caee0cb52f0a56183518
NoBeta1216 <60479205+Thura122016@users.noreply.github.com>
2025-05-29 05:34:48 +0630

    added ATAConfig.py
    Added Blank Config File for Github

---
+++ ATAConfig_.py
@@ -0,0 +1,16 @@
+# rename ATAConfig_.py to ATAConfig.py
+
+SMILE_UID = ""              # Merchant UserID
+SMILE_EMAIL = ""            # Merchant Email
+SMILE_KEY  = ""             # Merchant Key
+
+SMILE_URL = "https://www.smile.one"
+SMILE_API = {
+    "get_server_list": "/smilecoin/api/getserver",
+    "get_product": "/smilecoin/api/product",
+    "get_query_points": "/smilecoin/api/querypoints",
+    "get_product_list": "/smilecoin/api/productlist",
+    "get_role": "/smilecoin/api/getrole",
+    "create_order": "/smilecoin/api/createorder",
+    "get_role_mcgg": "/merchant/game/checkrole?product=magicchessgogo"
+}
